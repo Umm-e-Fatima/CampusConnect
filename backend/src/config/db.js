4f4533920f18ae38f4c2ff2f59348118d152bf1c
Umm-e-Fatima <fatimaanis809@gmail.com>
2026-07-21 09:32:08 +0500

    changes in env file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,20 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
   ssl: {
-  rejectUnauthorized: false,
-},
+    rejectUnauthorized: false,
+  },
 });
-
-
 
 pool.on('connect', () => console.log('Connected to PostgreSQL'));
 pool.on('error', (err) => { console.error('DB error:', err); process.exit(-1); });
